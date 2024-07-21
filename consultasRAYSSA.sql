ff318185112b5942408a2f476d74a336573f81e8
callmepeh <pedrosasada@gmail.com>
2024-07-21 16:47:50 -0300

    ajustes na colsulta rayssa

--- consultasRAYSSA.sql
+++ consultasRAYSSA.sql
@@ -1,6 +1,29 @@
+WITH CientistasMissoes AS (
+    SELECT f.ID_Funcionario, m.NomeMissao
+    FROM TBL_FUNCIONARIOS_TRIPULACAO f
+    JOIN TBL_CIENTISTAS c ON f.ID_Funcionario = c.ID_Funcionario
+    JOIN TBL_TRIPULACAO t ON f.ID_Tripulacao = t.ID_Tripulacao
+    JOIN TBL_MISSOES_TRIPULADAS m ON t.NomeMissao = m.NomeMissao
+),
+
+MissoesComDadosCientificos AS (
+    SELECT DISTINCT NomeMissao
+    FROM TABELA_DADOS_CIENTIFICOS
+),
+
+CientistasEmTodasMissoes AS (
+    SELECT ID_Funcionario
+    FROM CientistasMissoes
+    WHERE NomeMissao IN (SELECT NomeMissao FROM MissoesComDadosCientificos)
+    GROUP BY ID_Funcionario
+    HAVING COUNT(DISTINCT NomeMissao) = (SELECT COUNT(*) FROM MissoesComDadosCientificos)
+)
+
+SELECT ID_Funcionario
+FROM CientistasEmTodasMissoes;
 WITH NavesMissoes AS (
     SELECT ID_Nave, NomeMissao
-    FROM TBL_NAVE_MISSAO
+    FROM TBL_NOMEMISSAO_NAVE
 ),
 
 MTripuladas AS (
@@ -17,25 +40,3 @@ NavesEmTodasMissoes AS (
 
 SELECT ID_Nave
 FROM NavesEmTodasMissoes;
-WITH CientistasMissoes AS (
-    SELECT f.ID_Funcionario, m.NomeMissao
-    FROM TBL_FUNCIONARIOS_TRIPULACAO f
-    JOIN TBL_CIENTISTAS c ON f.ID_Funcionario = c.ID_Cientista
-    JOIN TBL_TRIPULACAO t ON f.ID_Funcionario = t.ID_Funcionario
-    JOIN TBL_MISSOES m ON t.ID_Missao = m.ID_Missao
-),
-
-MissoesComDadosCientificos AS (
-    SELECT NomeMissao
-    FROM DADOS_CIENTIFICOS
-),
-
-CientistasEmTodasMissoes AS (
-    SELECT ID_Funcionario
-    FROM CientistasMissoes
-    GROUP BY ID_Funcionario
-    HAVING COUNT(DISTINCT NomeMissao) = (SELECT COUNT(*) FROM MissoesComDadosCientificos)
-)
-
-SELECT ID_Funcionario
-FROM CientistasEmTodasMissoes;
