3fc70c957cc456302142451da7748238ce2101bb
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-08-03 18:54:02 -0300

    Update consultasVIVIANY.sql

--- consultasVIVIANY.sql
+++ consultasVIVIANY.sql
@@ -1,74 +1,53 @@
+@ -1,22 +1,74 @@
 -- Consultas utilizando Funções Agregadas
 
---1.Número total de engenheiros por especialização
-SELECT e.Tipo AS Especializacao,COUNT(f.ID_Funcionario) AS TotalEngenheiros
-FROM TABELA_FUNCIONÁRIOS f
-JOIN tbl_tipo_funcionario e ON f.id_tipo = e.ID_Tipo
-WHERE f.id_tipo = 2  -- Tipo 2 é Engenheiro
-GROUP BY e.Tipo;
+-- 1. Contar quantas naves estão inativas
+select count(*) as Naves_Inativas
+from interstellarinsight.tbl_naves as Inativas
+where Status = 'Desativada';
 
---2.Encontrar a média de orçamento das missões tripuladas
+-- 2. Selecionar o ID da menor estrela
+SELECT ID_CorpoCeleste AS ID_MenorEstrela
+FROM interstellarinsight.tbl_estrelas
+WHERE Tamanho = (SELECT MIN(Tamanho) FROM interstellarinsight.tbl_estrelas);
+
+-- 3. Calcular a média de orçamento das missões tripuladas
 SELECT AVG(Orçamento) AS MediaOrcamento
-FROM TABELA_DADOS_FINANCEIROS;
+FROM interstellarinsight.TABELA_DADOS_FINANCEIROS;
 
---3.Contar o número total de missões por ano
-SELECT YEAR(DataDeLancamento) AS Ano,COUNT(*) AS TotalMissao
-FROM TBL_MISSOES_TRIPULADAS
-GROUP BY YEAR(DataDeLancamento);
+-- 4. Selecionar o ID da estrela mais distante
+select ID_CorpoCeleste as ID_MaiorDistancia
+from interstellarinsight.tbl_estrelas
+where Distância = (select max(Distância) from interstellarinsight.tbl_estrelas);
 
-<<<<<<< HEAD
--- Contar quantas sondas estão ativas
-select count(*) as Sondas_Ativas
-from interstellarinsight.TBL_SONDAS as Ativas
-where Status = 'Ativa';
-
-=======
---SEM FUNÇÃO DE EXTRAÇÃO:
-
---.numero total de missões(tripuladas e não tripuladas)
+-- 5. Calcular o total de missões (tripuladas e não tripuladas)
 SELECT 
-    (SELECT COUNT(*) FROM TBL_MISSOES_TRIPULADAS) +
-    (SELECT COUNT(*) FROM TBL_MISSOES_NÃO_TRIPULADAS) AS TotalMissões
+    (SELECT COUNT(*) FROM interstellarinsight.TBL_MISSÕES_TRIPULADAS) +
+    (SELECT COUNT(*) FROM interstellarinsight.TBL_MISSOES_NÃO_TRIPULADAS) AS TotalMissoes
 ;
 
---4.Contar quantas naves estão inativas
-SELECT COUNT(*) AS Total_Naves_Inativas
-FROM TBL_NAVES
-WHERE Status = 'Desativada';
+-- 6. Encontrar a média de duração das missões tripuladas em dias
+SELECT AVG(Duração) AS MediaDuracao
+FROM interstellarinsight.tbl_missões_tripuladas;
 
-
---5.Encontrar a missão com a maior quantidade total de dados científicos coletados
-SELECT d.`NomeMissão`,SUM(d.`ID_dados_científicos`) AS TotalDados
-FROM tabela_dados_científicos d
-GROUP BY d.`NomeMissão`
-ORDER BY TotalDados DESC
-LIMIT 1;
-
---6.Encontrar a média de duração das missões tripuladas
--- Para calcular a média da duração das missões tripuladas em dias
-SELECT AVG(CAST(SUBSTRING_INDEX(Duracao, ' ', 1) AS DECIMAL)) AS MediaDuracao
-FROM TBL_MISSOES_TRIPULADAS;
-
---7.Encontrar a missão mais recente
-SELECT NomeMissao,MAX(DataDeLancamento) AS DataRecente
-FROM TBL_MISSOES_TRIPULADAS
-GROUP BY NomeMissao
-ORDER BY DataRecente DESC
-LIMIT 1;
-
---8.Contar o número total de cientistas por área de pesquisa
+-- 7. Contar o total de cientistas por área de pesquisa
 SELECT ÁreaDePesquisa, COUNT(*) AS TotalCientistas
-FROM TABELA_CIENTISTA
+FROM interstellarinsight.TABELA_CIENTISTA
 GROUP BY ÁreaDePesquisa;
 
---9.Encontrar a média de gastos das missões
+-- 8. Calcular a média de gastos das missões
 SELECT AVG(Gastos) AS MediaGastos
 FROM TABELA_DADOS_FINANCEIROS;
 
---10Encontrar a missão com o maior número de astronautas
-SELECT NomeMissão, COUNT(ID_Funcionario) AS TotalAstronautas
-FROM tbl_tripulação
+-- 9. Encontrar a missão com o maior número de astronautas
+SELECT NomeMissão, COUNT(ID_Funcionário) AS TotalAstronautas
+FROM interstellarinsight.tbl_tripulação
 GROUP BY NomeMissão
 ORDER BY TotalAstronautas DESC
 LIMIT 1;
->>>>>>> c378deccce5d74e060f5bd7f4c27a4bcdbc4c650
+
+-- 10. Encontrar a missão tripulada mais recente
+SELECT NomeMissão
+FROM interstellarinsight.tbl_missões_tripuladas
+ORDER BY DataDeLançamento DESC
+LIMIT 1;
